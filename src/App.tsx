42aab157cc66915e14fae7bd06efe4ead758923b
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-17 18:43:55 -0500

    Add better eslint rules for TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App(): React.FunctionComponentElement<{}> {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Zombie Roleplaying Application coming soon!</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
