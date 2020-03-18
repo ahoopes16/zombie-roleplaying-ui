af9df26665fdae87ec1b70b83f6910ad93477c8a
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-17 19:01:28 -0500

    Set up test environment with enzyme
    I prefer enzyme and I just wanted it :)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { shallow } from 'enzyme'
 import App from './App'
 
 test('renders without crashing', () => {
-  const app = render(<App />)
-  expect(app).toBeTruthy()
+  shallow(<App />)
 })
