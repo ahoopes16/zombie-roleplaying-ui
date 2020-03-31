d584eb113f8f35b413ec96b7f3396864df80253a
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-31 14:33:39 -0500

    Wrap Application in Initial Hash Router (#5)
    * Add react-router-dom as a dependency
    
    * Wrap the application in a basic hash router
    
    * Add a link from both forms to each other (mostly as an example)
    
    * Add a single lone test around ListEncounters

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './App.css'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import ListEncounters from './components/ListEncounters'
 import CreateEncounterForm from './components/CreateEncounterForm'
 
 function App(): React.FunctionComponentElement<{}> {
   return (
     <div className="App">
-      <ListEncounters />
-      <CreateEncounterForm />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={CreateEncounterForm} />
+          <Route exact path="/encounters" component={ListEncounters} />
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
     </div>
   )
 }
