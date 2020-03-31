398e232816ebed386f4bd71635c5dad59b66d703
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-30 23:14:06 -0500

    Add a single lone test around ListEncounters

---
+++ src/components/ListEncounters.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ListEncounters from './ListEncounters'
+
+// TODO Figure out how to do more tests
+describe('ListEncounters', () => {
+  test('renders', () => {
+    shallow(<ListEncounters />)
+  })
+})
