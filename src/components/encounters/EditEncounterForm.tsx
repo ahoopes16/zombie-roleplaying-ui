26ce397dd8898182f0df52b784b7b7d45d25104e
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-04-03 16:54:58 -0500

    Generate the starter form for Updating an Encounter

--- src/components/encounters/EditEncounterForm.tsx
+++ src/components/encounters/EditEncounterForm.tsx
@@ -2,6 +2,20 @@ import React, { FunctionComponentElement, useState, useEffect } from 'react'
 import { RouterProps } from '../../types/reactRouterProps'
 import { Encounter } from '../../types/encounter.type'
 import api from '../../api'
+import {
+  Alert,
+  Spinner,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Form,
+  Col,
+  FormGroup,
+  Input,
+  FormText,
+  Button
+} from 'reactstrap'
 
 function EditEncounterForm(
   props: RouterProps
@@ -23,9 +37,66 @@ function EditEncounterForm(
     fetchEncounter()
   }, [id])
 
-  console.error(error)
-  console.log(encounter?.title)
-  return <p>{encounter?.title}</p>
+  if (error) {
+    return (
+      <Alert className="error-container" color="danger">
+        {error.message}
+      </Alert>
+    )
+  }
+
+  if (!encounter) {
+    return <Spinner color="primary" />
+  }
+
+  const isDisabled = (): boolean => !encounter.title || !encounter.description
+
+  return (
+    <Card className="form-container text-center">
+      <CardHeader>
+        <CardTitle tag="h2">Edit Encounter</CardTitle>
+      </CardHeader>
+
+      <CardBody>
+        <Form>
+          <Col>
+            <FormGroup className="form-field">
+              <Input
+                id="create-encounter-title-input"
+                type="text"
+                placeholder="Title"
+                value={encounter.title}
+                onChange={(e): string =>
+                  (encounter.title = e.currentTarget.value)
+                }
+              />
+              <FormText color="muted" className="float-left">
+                Please make sure the title is unique!
+              </FormText>
+            </FormGroup>
+          </Col>
+
+          <Col>
+            <FormGroup className="form-field">
+              <Input
+                id="create-encounter-description-input"
+                type="textarea"
+                placeholder="Description"
+                value={encounter.description}
+                onChange={(e): string =>
+                  (encounter.description = e.currentTarget.value)
+                }
+              />
+            </FormGroup>
+          </Col>
+
+          <Button color="primary" disabled={isDisabled()}>
+            Update Encounter
+          </Button>
+        </Form>
+      </CardBody>
+    </Card>
+  )
 }
 
 export default EditEncounterForm
